aab78dc1b92dd7d7d229ce7c9ba811e6aa5d917e
Adam Jonsson <adamsmajl@gmail.com>
2021-01-28 12:07:45 +0100

    Add skeleton for LIC conditions and tests (#9)
    Fixes #7

---
+++ src/lab1/LaunchInterceptorConditions.java
@@ -0,0 +1,63 @@
+package lab1;
+
+public class LaunchInterceptorConditions {
+    public static boolean condition0(int[] listPoints, int len) {
+        return false;
+    }
+
+    public static boolean condition1() {
+        return false;
+    }
+
+    public static boolean condition2() {
+        return false;
+    }
+
+    public static boolean condition3() {
+        return false;
+    }
+
+    public static boolean condition4() {
+        return false;
+    }
+
+    public static boolean condition5() {
+        return false;
+    }
+
+    public static boolean condition6() {
+        return false;
+    }
+
+    public static boolean condition7() {
+        return false;
+    }
+
+    public static boolean condition8() {
+        return false;
+    }
+
+    public static boolean condition9() {
+        return false;
+    }
+
+    public static boolean condition10() {
+        return false;
+    }
+
+    public static boolean condition11() {
+        return false;
+    }
+
+    public static boolean condition12() {
+        return false;
+    }
+
+    public static boolean condition13() {
+        return false;
+    }
+
+    public static boolean condition14() {
+        return false;
+    }
+}
---
+++ src/lab1/test/LaunchInterceptorConditionsTest.java
@@ -0,0 +1,15 @@
+package lab1.test;
+
+import org.junit.jupiter.api.Test;
+import lab1.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LaunchInterceptorConditionsTest {
+    // Dummy test
+    @Test
+    void co1() {
+        LaunchInterceptorConditions.condition0(new int[2], 20);
+        assertEquals(true, true);
+    }
+}
