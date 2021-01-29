5cab6c834b7ee3fdf1be27745996cebcdbb567e4
Adam Jonsson <adamsmajl@gmail.com>
2021-01-29 15:32:00 +0100

    Add helper function for angle between points

--- src/lab1/test/LaunchInterceptorConditionsTest.java
+++ src/lab1/test/LaunchInterceptorConditionsTest.java
@@ -6,10 +6,41 @@ import lab1.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LaunchInterceptorConditionsTest {
-    // Dummy test
+
+    /** Should return false if epsilon is not a valid value */
     @Test
-    void co1() {
-        LaunchInterceptorConditions.condition0(new int[2], 20);
-        assertEquals(true, true);
+    void angleBetweenPointsEpsilonNotValid() {
+        double[] x = new double[]{-2, 0, 2};
+        double[] y = new double[]{1, 0, 1};
+        var result = LaunchInterceptorConditions.angleBetweenPointsOutsideEpsilon(x[0], y[0], x[1], y[1], x[2], y[2], 4);
+        assertEquals(false, result);
     }
+
+    /** Should return false if the angle between the points is not valid */
+    @Test
+    void angleBetweenAngleNoValid() {
+        double[] x = new double[]{0, 0, 2};
+        double[] y = new double[]{0, 0, 1};
+        var result = LaunchInterceptorConditions.angleBetweenPointsOutsideEpsilon(x[0], y[0], x[1], y[1], x[2], y[2], 4);
+        assertEquals(false, result);
+    }
+
+    /** Should return false as the angle is within the epsilon range */
+    @Test
+    void angleBetweenAngleWithinEpsilon() {
+        double[] x = new double[]{-1, 0, 1};
+        double[] y = new double[]{0, 0, 0};
+        var result = LaunchInterceptorConditions.angleBetweenPointsOutsideEpsilon(x[0], y[0], x[1], y[1], x[2], y[2], Math.PI / 4);
+        assertEquals(false, result);
+    }
+
+    /** Should return true as the angle is outside the epsilon range */
+    @Test
+    void angleBetweenAngleOutsideEpsilon() {
+        double[] x = new double[]{-1, 0, 1};
+        double[] y = new double[]{1, 0, 1};
+        var result = LaunchInterceptorConditions.angleBetweenPointsOutsideEpsilon(x[0], y[0], x[1], y[1], x[2], y[2], Math.PI / 3);
+        assertEquals(true, result);
+    }
+
 }
