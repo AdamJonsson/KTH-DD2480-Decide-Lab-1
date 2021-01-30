0544ea7450c342e1101feed123f2855025ff79c1
Autonymic <75275746+Autonymic@users.noreply.github.com>
2021-01-30 23:57:04 +0100

    Added tests for LIC condition 10

--- src/lab1/test/LaunchInterceptorConditionsTest.java
+++ src/lab1/test/LaunchInterceptorConditionsTest.java
@@ -54,4 +54,33 @@ public class LaunchInterceptorConditionsTest {
         boolean result = LaunchInterceptorConditions.condition9(x, y, c_pts, d_pts, epsilon, 4);
         assertEquals(result, false);
     }
+
+    /**
+     * Should return true if the set of 3 data points separated by exactly e_pts and f_pts
+     * consecutive intervening points form a triangle with area greater than AREA1
+     */
+    @Test
+    void co10_1 {
+        double[] x = new double[] {0, 2, 3, 2, 4, 5, 4};
+        double[] y = new double[] {0, 2, 3, 5, 4, 5, 0};
+        e_pts = 2;
+        f_pts = 2;
+        area1 = 0;
+        boolean result = LaunchInterceptorConditions.condition10(x, y, e_pts, f_pts, area1, 7);
+        assertEquals(result, true);
+    }
+
+    /**
+     * Should return false if NUMPOINTS < 5
+     */
+    @Test
+    void co10_2 {
+        double[] x = new double[] {0, 0, 0, 0};
+        double[] y = new double[] {0, 0, 0, 0};
+        e_pts = 0;
+        f_pts = 0;
+        area1 = 0;
+        boolean result = LaunchInterceptorConditions.condition10(x, y, e_pts, f_pts, area1, 4);
+        assertEquals(result, false);
+    }
 }
