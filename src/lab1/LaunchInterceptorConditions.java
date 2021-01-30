46b76bd85f9ef535a758cdd12c88f7989f6bf5cc
Adam Jonsson <adamsmajl@gmail.com>
2021-01-30 11:43:33 +0100

    Add helper function for angle between points (#20)
    * Add helper function for angle between points
    
    * Change variable type from var to boolean
    
    * Fix typo in comments
    
    * Take use of built in Math.hypot func

--- src/lab1/LaunchInterceptorConditions.java
+++ src/lab1/LaunchInterceptorConditions.java
@@ -77,4 +77,33 @@ public class LaunchInterceptorConditions {
     public static boolean condition14() {
         return false;
     }
+
+    /**
+     * Returns true if the following conditions are true: angle < (PI - epsilon)
+     * or angle > (PI + epsilon). The second point of the three points is always the
+     * vertex of the angle.
+     * 
+     * @param x1      x position of first point
+     * @param y1      y position of first point
+     * @param x2      x position of second point
+     * @param y2      y position of second point
+     * @param x3      x position of third point
+     * @param y3      y position of third point
+     * @param epsilon
+     * @return True or false if conditions are met
+     */
+    public static boolean angleBetweenPointsOutsideEpsilon(double x1, double y1, double x2, double y2, double x3,
+            double y3, double epsilon) {
+        if (epsilon >= Math.PI)
+            return false;
+        if ((x1 == x2 && y1 == y2) || (x3 == x2 && y3 == y2))
+            return false;
+
+        double distanceBToA = Math.hypot(x2 - x1, y2 - y1);
+        double distanceBToC = Math.hypot(x2 - x3, y2 - y3);
+        double distanceAToC = Math.hypot(x1 - x3, y1 - y3);
+        double angleInRadians = Math.acos((Math.pow(distanceBToA, 2) + Math.pow(distanceBToC, 2) - Math.pow(distanceAToC, 2))
+                / (2 * distanceBToA * distanceBToC));
+        return angleInRadians < (Math.PI - epsilon) || angleInRadians > (Math.PI + epsilon);
+    }
 }
--- src/lab1/test/LaunchInterceptorConditionsTest.java
+++ src/lab1/test/LaunchInterceptorConditionsTest.java
@@ -37,4 +37,41 @@ public class LaunchInterceptorConditionsTest {
         assertEquals(LaunchInterceptorConditions.triangleAreaIsGreaterThanArea(x1, y1, x2, y2, x3, y3, 0.55), false);
         assertEquals(LaunchInterceptorConditions.triangleAreaIsGreaterThanArea(x1, y1, x2, y2, x3, y3, 0.45), true);
     }
+
+    /** Should return false if epsilon is not a valid value */
+    @Test
+    void angleBetweenPointsEpsilonNotValid() {
+        double[] x = new double[]{-2, 0, 2};
+        double[] y = new double[]{1, 0, 1};
+        boolean result = LaunchInterceptorConditions.angleBetweenPointsOutsideEpsilon(x[0], y[0], x[1], y[1], x[2], y[2], 4);
+        assertEquals(false, result);
+    }
+
+    /** Should return false if the angle between the points is not valid */
+    @Test
+    void angleBetweenAngleNoValid() {
+        double[] x = new double[]{0, 0, 2};
+        double[] y = new double[]{0, 0, 1};
+        boolean result = LaunchInterceptorConditions.angleBetweenPointsOutsideEpsilon(x[0], y[0], x[1], y[1], x[2], y[2], 4);
+        assertEquals(false, result);
+    }
+
+    /** Should return false as the angle is within the epsilon range */
+    @Test
+    void angleBetweenAngleWithinEpsilon() {
+        double[] x = new double[]{-1, 0, 1};
+        double[] y = new double[]{0, 0, 0};
+        boolean result = LaunchInterceptorConditions.angleBetweenPointsOutsideEpsilon(x[0], y[0], x[1], y[1], x[2], y[2], Math.PI / 4);
+        assertEquals(false, result);
+    }
+
+    /** Should return true as the angle is outside the epsilon range */
+    @Test
+    void angleBetweenAngleOutsideEpsilon() {
+        double[] x = new double[]{-1, 0, 1};
+        double[] y = new double[]{1, 0, 1};
+        boolean result = LaunchInterceptorConditions.angleBetweenPointsOutsideEpsilon(x[0], y[0], x[1], y[1], x[2], y[2], Math.PI / 3);
+        assertEquals(true, result);
+    }
+
 }
