0c1ae7ad7230591511f875cddd28c7c2ac400e42
larasm <larasm@kth.se>
2021-01-31 22:50:54 +0100

    Add condition 12

--- src/lab1/LaunchInterceptorConditions.java
+++ src/lab1/LaunchInterceptorConditions.java
@@ -49,8 +49,44 @@ public class LaunchInterceptorConditions {
         return false;
     }
 
-    public static boolean condition12(double[] x, double[] y, int kPts, double length1, double length2, int numPoints) {
-        return false;
+    /**
+     * Returns true if there exists at least one set of K PTS consecutive data points that are a distance of more than length1 apart
+     * and another set of data that are a distance of more than length2 apart
+     * 
+     * @param x
+     * @param y
+     * @param length1
+     * @param length2
+     * @param kPts
+     * @return True of false
+     */
+
+    public static boolean condition12(double[] x, double[] y, double length1, double length2, int kPts) {
+        boolean length1Fulfilled = false;
+        boolean length2Fulfilled = false;
+        // We need kPts intervening points: if kpts = 2, this means we need two points between (x1, y1) and (x2, y2)
+        // meaning we need to set the separation to kPts + 1
+        kPts += 1;
+        if (x.length < kPts || y.length < kPts) {
+            return false;
+        }
+
+        for (int i = 0; i < x.length - kPts; i += kpts) {
+            double x1 = x[i];
+            double y1 = y[i];
+
+            double x2 = x[i + kPts];
+            double y2 = y[i + kPts];
+
+            if helperDistance(x1, y1, x2, y2, length1) {
+                length1Fulfilled = true;
+            }
+
+            if helperDistance(x1, y1, x2, y2, length2) {
+                length2Fulfilled = true;
+            }
+        }
+        return (length1Fulfilled && length2Fulfilled);
     }
 
     public static boolean condition13(double[] x, double[] y, int aPts, int bPts, double radius1, double radius2, int numPoints) {
