366b7ccf37c721b16940fb05d6deee2ae7b3d393
Hovig <hoffek@kth.se>
2021-02-01 18:51:05 +0100

    Add tests to the PUM class

---
+++ src/lab1/PUM.java
@@ -0,0 +1,17 @@
+package lab1;
+
+public class PUM {
+    public PUM(boolean[] cvm, int[][] lcm){
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Gets the value of the (i,j) cell in the PUM matrix.
+     * @param i the row number.
+     * @param j the column number.
+     * @return the boolean value at (i,j).
+     */
+    public boolean get(int i, int j){
+        throw new UnsupportedOperationException();
+    }
+}
