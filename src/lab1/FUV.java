b3585766d20cfa624f7c4e196234ce5e7730ee3e
larasm <larasm@kth.se>
2021-02-02 19:24:55 +0100

    Add skeleton for FUV class

---
+++ src/lab1/FUV.java
@@ -0,0 +1,29 @@
+/**
+ * DD2480-VT21-1
+ * Assignment #1: DECIDE
+ *
+ * @autors Adam Jonsson, Hovig Manjikian, Isak Vilhelmsson, Lara Rostami, Tony Le
+ * @version 1.0
+ * @since 02-02-2021
+ */
+
+package lab1;
+
+public class FUV {
+    // TODO: add commentand fix program
+    /**
+     *
+     * @param PUV  
+     * @param pumMatrix
+     * @return
+     */
+
+    public FUV(boolean[] PUV, PUM pumMatrix) {
+
+    }
+
+    public get(int i) {
+        return false;
+    }
+
+}
