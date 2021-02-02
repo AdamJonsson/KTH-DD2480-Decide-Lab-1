4c218540e20c5191601858cb1b4ba0957bfb85de
Autonymic <75275746+Autonymic@users.noreply.github.com>
2021-02-02 03:40:53 +0100

    Added angle condition to condition 9

--- src/lab1/LaunchInterceptorConditions.java
+++ src/lab1/LaunchInterceptorConditions.java
@@ -57,173 +57,32 @@ public class LaunchInterceptorConditions {
     }
 
     /**
-     * There exists at least one set of two consecutive data points that are a distance greater than
-     * the length, LENGTH1, apart.
+     * Returns true if the following conditions are true: angle < (PI - epsilon)
+     * or angle > (PI + epsilon). The second point of the three points is always the
+     * vertex of the angle.
      *
+     * @param x1      x position of first point
+     * @param y1      y position of first point
+     * @param x2      x position of second point
+     * @param y2      y position of second point
+     * @param x3      x position of third point
+     * @param y3      y position of third point
+     * @param epsilon
+     * @return True or false if conditions are met
      */
-    public static boolean condition0(double[] xList, double[] yList, double lenght1) {
-        if (xList.length != yList.length || lenght1 < 0)
+    public static boolean angleBetweenPointsOutsideEpsilon(double x1, double y1, double x2, double y2, double x3,
+                                                           double y3, double epsilon) {
+        if (epsilon >= Math.PI)
+            return false;
+        if ((x1 == x2 && y1 == y2) || (x3 == x2 && y3 == y2))
             return false;
 
-        for (int i = 0; i < xList.length-1; i++) {
-            boolean result = helperDistance(xList[i], yList[i], xList[i+1], yList[i+1], lenght1);
-            if (result)
-                return true;
-        }
-        return false;
-    }
-
-    /**
-     * There exists at least one set of three consecutive data points that cannot all be contained
-     * within or on a circle of radius RADIUS1.
-     *
-     */
-    public static boolean condition1(double[] xList, double[] yList, double radius1) {
-        if (xList.length != yList.length || radius1 < 0)
-            return false;
-
-        for (int i = 0; i < xList.length-2; i++) {
-            boolean result = helperCircle(xList[i], yList[i], xList[i+1], yList[i+1], xList[i+2], yList[i+2], radius1);
-            if (result)
-                return true;
-        }
-        return false;
-    }
-
-    /**
-     * There exists at least one set of three consecutive data points which form an angle such that:
-     * angle < (PI−EPSILON)
-     * or
-     * angle > (PI+EPSILON)
-     * The second of the three consecutive points is always the vertex of the angle. If either the first
-     * point or the last point (or both) coincides with the vertex, the angle is undefined and the LIC
-     * is not satisfied by those three points.
-     *
-     * @return
-     */
-    public static boolean condition2(double[] xList, double[] yList, double epsilon) {
-        for (int i = 0; i < xList.length-2; i++) {
-            boolean result = angleBetweenPointsOutsideEpsilon(xList[i], yList[i], xList[i+1], yList[i+1], xList[i+2], yList[i+2], epsilon);
-            if (result)
-                return true;
-        }
-        return false;
-    }
-
-    /**
-     * There exists at least one set of three consecutive data points that are the vertices of a triangle 
-     * with area greater than AREA1.
-     * 
-     * @param x
-     * @param y
-     * @param area1
-     * @return
-     */
-    public static boolean condition3(double[] x, double[] y, double area1) {
-        if (x.length != y.length)
-            return false;
-
-        if (x.length < 3)
-            return false;
-
-        if (area1 < 0)
-            throw new IllegalArgumentException("Area can not be negative");
-        
-        for (int i = 0; i < y.length - 2; i++) {
-            boolean conditionMet = triangleAreaIsGreaterThanArea(x[i], y[i], x[i + 1], y[i + 1], x[i + 2], y[i + 2], area1);
-            if (conditionMet)
-                return true;
-        }
-        return false;
-    }
-
-    /**
-     * Returns true if there exists at least one set of Q PTS consecutive data points that lie in more than QUADS quadrants
-     * 
-     * @param x
-     * @param y
-     * @param quads
-     * @param qPts
-     * @return True of false
-     */
-    public static boolean condition4(double[] x, double[] y, int quads, int qPts) {
-        
-        if (qPts < 2 || qPts > x.length)
-            return false;
-        
-        if (quads > 3 || quads < 1)
-            throw new IllegalArgumentException("quads must be between the value 1 and 3 inclusive");
-        
-        for (int i = 0; i <= x.length - qPts; i++) {
-            boolean[] pointsExistInQuadrant = new boolean[] {false, false, false, false};
-            for (int qPtsIndex = 0; qPtsIndex < qPts; qPtsIndex++) {
-                double xPos = x[qPtsIndex + i];
-                double yPos = y[qPtsIndex + i];
-
-                // Check if point is in quadrant 1
-                if (xPos >= 0 && yPos >= 0) {
-                    pointsExistInQuadrant[0] = true;
-                }
-
-                // Check if point is in quadrant 2
-                else if (xPos < 0 && yPos >= 0) {
-                    pointsExistInQuadrant[1] = true;
-                }
-                
-                // Check if point is in quadrant 3
-                else if (xPos < 0 && yPos < 0) {
-                    pointsExistInQuadrant[2] = true;
-                }
-
-                // Check if point is in quadrant 4
-                else if (xPos > 0 && yPos < 0) {
-                    pointsExistInQuadrant[3] = true;
-                }
-            }
-
-            int numConsecutivePointsInDifferentQuadrants = 0;
-            for (boolean inQuadrant : pointsExistInQuadrant) {
-                if (inQuadrant) {
-                    numConsecutivePointsInDifferentQuadrants++;
-                }
-            }
-
-            if (numConsecutivePointsInDifferentQuadrants > quads)
-                return true;
-        }
-        return false;
-    }
-
-    /**
-     * There exists at least one set of two consecutive data points, (X[i],Y[i]) and (X[j],Y[j]), such
-     * that X[j] - X[i] < 0. (where i = j-1)
-     * 
-     * @param x
-     * @param y
-     * @return True if the condition is met, otherwise false
-     */
-    public static boolean condition5(double[] x, double[] y) {
-        if (x.length != y.length || x.length < 2)
-            return false;
-
-        for (int i = 0; i < x.length - 1; i++) {
-            if (x[i + 1] - x[i] < 0)
-                return true;
-        }
-
-        return false;
-    }
-
-    public static boolean condition6() {
-        return false;
-    }
-
-    public static boolean condition7() {
-        return false;
-    }
-
-    public static boolean condition8() {
-        return false;
+        double distanceBToA = Math.hypot(x2 - x1, y2 - y1);
+        double distanceBToC = Math.hypot(x2 - x3, y2 - y3);
+        double distanceAToC = Math.hypot(x1 - x3, y1 - y3);
+        double angleInRadians = Math.acos((Math.pow(distanceBToA, 2) + Math.pow(distanceBToC, 2) - Math.pow(distanceAToC, 2))
+                / (2 * distanceBToA * distanceBToC));
+        return angleInRadians < (Math.PI - epsilon) || angleInRadians > (Math.PI + epsilon);
     }
 
     public static boolean condition9(double[] x, double[] y, int c_pts, int d_pts, double epsilon, int numPoints) {
@@ -239,6 +98,10 @@ public class LaunchInterceptorConditions {
             return false;
         }
 
+        if (!angleBetweenPointsOutsideEpsilon(x[firstIndex], y[firstIndex], x[vertexIndex], y[vertexIndex], x[lastIndex], y[lastIndex], epsilon)) {
+            return false;
+        }
+
         return true;
     }
 
@@ -248,46 +111,5 @@ public class LaunchInterceptorConditions {
 
     public static boolean condition11(double[] x, double[] y, int g_pts, int numPoints) {
         return false;
-    }
-
-    public static boolean condition12() {
-        return false;
-    }
-
-    public static boolean condition13() {
-        return false;
-    }
-
-    public static boolean condition14() {
-        return false;
-    }
-
-    /**
-     * Returns true if the following conditions are true: angle < (PI - epsilon)
-     * or angle > (PI + epsilon). The second point of the three points is always the
-     * vertex of the angle.
-     * 
-     * @param x1      x position of first point
-     * @param y1      y position of first point
-     * @param x2      x position of second point
-     * @param y2      y position of second point
-     * @param x3      x position of third point
-     * @param y3      y position of third point
-     * @param epsilon
-     * @return True or false if conditions are met
-     */
-    public static boolean angleBetweenPointsOutsideEpsilon(double x1, double y1, double x2, double y2, double x3,
-            double y3, double epsilon) {
-        if (epsilon >= Math.PI)
-            return false;
-        if ((x1 == x2 && y1 == y2) || (x3 == x2 && y3 == y2))
-            return false;
-
-        double distanceBToA = Math.hypot(x2 - x1, y2 - y1);
-        double distanceBToC = Math.hypot(x2 - x3, y2 - y3);
-        double distanceAToC = Math.hypot(x1 - x3, y1 - y3);
-        double angleInRadians = Math.acos((Math.pow(distanceBToA, 2) + Math.pow(distanceBToC, 2) - Math.pow(distanceAToC, 2))
-                / (2 * distanceBToA * distanceBToC));
-        return angleInRadians < (Math.PI - epsilon) || angleInRadians > (Math.PI + epsilon);
     }
 }
--- src/lab1/test/LaunchInterceptorConditionsTest.java
+++ src/lab1/test/LaunchInterceptorConditionsTest.java
@@ -6,320 +6,6 @@ import lab1.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class LaunchInterceptorConditionsTest {
-
-    /**
-     * Testing the function for LIC 3 with a set of data that contains a valid triangle with area larger
-     * than AREA1. All points have positive coordinates.
-     */
-    @Test
-    void lic3_ValidInput_NoneNegativeCoordinates() {
-        double[] x = new double[]{0, 2, 5, 6};
-        double[] y = new double[]{0, 0, 2, 0};
-        double area1 = 3;
-        assertTrue(LaunchInterceptorConditions.condition3(x, y, area1));
-    }
-
-    /**
-     * Testing the function for LIC 3 with a set of data that contains a valid triangle with area larger
-     * than AREA1. Some of the points have negative coordinates.
-     */
-    @Test
-    void lic3_ValidInput_NegativeCoordinates() {
-        double[] x = new double[]{-2, 1, 2};
-        double[] y = new double[]{-2, 1, -2};
-        double area1 = 5;
-        assertTrue(LaunchInterceptorConditions.condition3(x, y, area1));
-        x = new double[]{-1, 0, 3};
-        y = new double[]{0.5, -2.25, 0};
-        area1 = 5.1;
-        assertTrue(LaunchInterceptorConditions.condition3(x, y, area1));
-    }
-
-    /**
-     * Testing the function for LIC 3 with a set of data that contains many valid triangles but none of
-     * the triangles have an area larger than AREA1.
-     */
-    @Test
-    void lic3_ValidInput_NoTriangleGreaterThanArea1() {
-        double[] x = new double[]{0, 2, 5, 6};
-        double[] y = new double[]{0, 0, 2, 0};
-        double area1 = 5;
-        assertFalse(LaunchInterceptorConditions.condition3(x, y, area1));
-    }
-
-    /**
-     * Testing the function for LIC 3 with a set of data that hase less than 3 points. So there cannot
-     * exist any triangle.
-     */
-    @Test
-    void lic3_ValidInput_LessThanThreePoints() {
-        double[] x = new double[]{0, 2};
-        double[] y = new double[]{0, 0};
-        double area1 = 0.01;
-        assertFalse(LaunchInterceptorConditions.condition3(x, y, area1));
-        x = new double[]{0};
-        y = new double[]{0};
-        assertFalse(LaunchInterceptorConditions.condition3(x, y, area1));
-        x = new double[]{};
-        y = new double[]{};
-        assertFalse(LaunchInterceptorConditions.condition3(x, y, area1));
-    }
-
-    /**
-     * Testing the function for LIC 3 with a set of data that do not contain any triangle.
-     */
-    @Test
-    void lic3_ValidInput_NoTriangle() {
-        double[] x = new double[]{0, 2, 5, 6};
-        double[] y = new double[]{1, 1, 1, 1};
-        double area1 = 0.01;
-        assertFalse(LaunchInterceptorConditions.condition3(x, y, area1));
-        x = new double[]{2, 2, 2, 2};
-        y = new double[]{2, 2, 2, 2};
-        assertFalse(LaunchInterceptorConditions.condition3(x, y, area1));
-    }
-
-    /**
-     * Testing the function for LIC 3 with an invalid input of a negative AREA1.
-     */
-    @Test
-    void lic3_InvalidInput_NegativeArea1() {
-        double[] x = new double[]{-2, 1, 2};
-        double[] y = new double[]{-2, 1, -2};
-        double area1 = -5;
-        assertThrows(IllegalArgumentException.class, () -> LaunchInterceptorConditions.condition3(x, y, area1));
-    }
-
-    /**
-     * Testing a sequence of points that contain Q_PTS consecutive points in more than QUADS quadrants.
-     */
-    @Test
-    void lic4_ValidInput_Contains() {
-        double[] x = new double[]{1, 1, -1, 3};
-        double[] y = new double[]{1, 3, 2, 2};
-        int quads = 1;
-        assertTrue(LaunchInterceptorConditions.condition4(x, y, quads, x.length));
-        x = new double[]{1, 1.5, -1, 3, 3};
-        y = new double[]{1, 3.2, 2, 2, -2};
-        quads = 2;
-        assertTrue(LaunchInterceptorConditions.condition4(x, y, quads, x.length));
-        x = new double[]{-1, 1, -1, 1};
-        y = new double[]{-1, 1, 1, -1};
-        quads = 3;
-        assertTrue(LaunchInterceptorConditions.condition4(x, y, quads, x.length));
-    }
-
-    /**
-     * Testing a sequence of points that do not contain Q_PTS consecutive
-     * points in more than QUADS quadrants.
-     */
-    @Test
-    void lic4_ValidInput_DoesNotContain() {
-        double[] x = new double[]{1, 1, -1, 3};
-        double[] y = new double[]{1, 3, 2, 2};
-        int quads = 2;
-        assertFalse(LaunchInterceptorConditions.condition4(x, y, quads, x.length));
-        x = new double[]{1, 1.5, -1, 3, 3};
-        y = new double[]{1, 3.2, 2, 2, -2};
-        quads = 3;
-        assertFalse(LaunchInterceptorConditions.condition4(x, y, quads, x.length));
-        x = new double[]{-1, -2, -1, -4};
-        y = new double[]{-1, -1, -3, -1};
-        quads = 1;
-        assertFalse(LaunchInterceptorConditions.condition4(x, y, quads, x.length));
-        x = new double[]{1, 2, -1, -4, -3, -2, 1, 4};
-        y = new double[]{1, 1, 3, 1, -2, -2, -5, -1};
-        quads = 2;
-        assertFalse(LaunchInterceptorConditions.condition4(x, y, quads, 2));
-    }
-
-    /**
-     * Testing the ambiguous cases when a point is in between two or more quadrants.
-     */
-    @Test
-    void lic4_ValidInput_AmbiguousCases() {
-        double[] x = new double[]{0, 2, 1};
-        double[] y = new double[]{0, 1, 3};
-        int quads = 1;
-        assertFalse(LaunchInterceptorConditions.condition4(x, y, quads, x.length));
-        x = new double[]{0, -2.2, 1};
-        y = new double[]{0, 1, -3};
-        quads = 3;
-        assertFalse(LaunchInterceptorConditions.condition4(x, y, quads, x.length));
-        quads = 2;
-        assertTrue(LaunchInterceptorConditions.condition4(x, y, quads, x.length));
-        x = new double[]{-3, -2.2, -1};
-        y = new double[]{0, 1, 2};
-        quads = 1;
-        assertFalse(LaunchInterceptorConditions.condition4(x, y, quads, x.length));
-        x = new double[]{0, -2, -1};
-        y = new double[]{-3, -1, -2};
-        quads = 1;
-        assertFalse(LaunchInterceptorConditions.condition4(x, y, quads, x.length));
-    }
-
-    /**
-     * Testing an invalid input when QUADS is less than 1 or more than 3.
-     */
-    @Test
-    void lic4_InvalidInput_InvalidQuads() {
-        double[] x = new double[]{0, 2, 1};
-        double[] y = new double[]{0, 1, 3};
-        int quads1 = 0;
-        assertThrows(IllegalArgumentException.class, () -> LaunchInterceptorConditions.condition4(x, y, quads1, x.length));
-        int quads2 = 4;
-        assertThrows(IllegalArgumentException.class, () -> LaunchInterceptorConditions.condition4(x, y, quads2, x.length));
-    }
-
-    /**
-     * Testing with a valid input that contains two consecutive data points
-     * that fulfills X[j] - X[i] < 0.
-     */
-    @Test
-    void lic5_ValidInput_ContainsTwoPoint() {
-        double[] x = new double[]{1.25, 4, 2, 3};
-        double[] y = new double[]{1, 1, 1, 1};
-        assertTrue(LaunchInterceptorConditions.condition5(x, y));
-    }
-
-    /**
-     * Testing with a valid input that do not contain two consecutive data points
-     * that fulfills X[j] - X[i] < 0.
-     */
-    @Test
-    void lic5_ValidInput_DoNotContainsTwoPoint() {
-        double[] x = new double[]{1, 2, 3};
-        double[] y = new double[]{1, 1, 1};
-        assertFalse(LaunchInterceptorConditions.condition5(x, y));
-        x = new double[]{1, 2, 2};
-        assertFalse(LaunchInterceptorConditions.condition5(x, y));
-    }
-
-
-    /**
-     * Testing with a valid input that contains less than two data points.
-     */
-    @Test
-    void lic5_ValidInput_LessThanTwoPoints() {
-        double[] x = new double[]{1};
-        double[] y = new double[]{1};
-        assertFalse(LaunchInterceptorConditions.condition5(x, y));
-        x = new double[]{};
-        y = new double[]{};
-        assertFalse(LaunchInterceptorConditions.condition5(x, y));
-    }
-    
-    /**
-     * Testing two valid points and a valid LENGTH1.
-     */
-    @Test
-    void distance_ValidInput_GreaterThan() {
-        double x1 = 1.5;
-        double y1 = 1;
-        double x2 = 5;
-        double y2 = 1;
-        double length1 = 3.2;
-        assertTrue(LaunchInterceptorConditions.helperDistance(x1, y1, x2, y2, length1));
-        length1 = 3.6;
-        assertFalse(LaunchInterceptorConditions.helperDistance(x1, y1, x2, y2, length1));
-        x1 = 1.5;
-        y1 = -2;
-        x2 = -2.2;
-        y2 = 3;
-        length1 = 6.2;
-        assertTrue(LaunchInterceptorConditions.helperDistance(x1, y1, x2, y2, length1));
-        length1 = 6.3;
-        assertFalse(LaunchInterceptorConditions.helperDistance(x1, y1, x2, y2, length1));
-    }
-
-    /**
-     * Testing and invalid input of negative LENGTH1.
-     */
-    @Test
-    void distance_InvalidInput_NegativeLength1() {
-        double x1 = 1.5;
-        double y1 = 1;
-        double x2 = 5;
-        double y2 = 1;
-        double length1 = -3;
-        assertThrows(IllegalArgumentException.class, () -> LaunchInterceptorConditions.helperDistance(x1, y1, x2, y2, length1));
-    }
-
-    /**
-     * Testing with a valid input of three points that make a triangle which fits into/on a circle of radius r.
-     */
-    @Test
-    void threePoints_ValidInput_HaveTriangle() {
-        double x1 = 1;
-        double y1 = 0;
-        double x2 = 0;
-        double y2 = 1;
-        double x3 = -1;
-        double y3 = 0;
-        double r = 1;
-        assertTrue(LaunchInterceptorConditions.helperCircle(x1, y1, x2, y2, x3, y3, r));
-        x1 = 1.2;
-        y1 = 1.5;
-        x2 = 1.2;
-        y2 = 2.5;
-        x3 = 2.2;
-        y3 = 1.5;
-        assertTrue(LaunchInterceptorConditions.helperCircle(x1, y1, x2, y2, x3, y3, r));
-        x1 = 0.7071;
-        y1 = 0.7071;
-        x2 = -0.5;
-        y2 = 0.866;
-        x3 = -0.7071;
-        y3 = -0.7071;
-        assertTrue(LaunchInterceptorConditions.helperCircle(x1, y1, x2, y2, x3, y3, r));
-        x1 = 3.031;
-        y1 = 1.75;
-        x2 = 1.75;
-        y2 = 3.031;
-        x3 = -1.75;
-        y3 = -3.031;
-        r = 3.5;
-        assertTrue(LaunchInterceptorConditions.helperCircle(x1, y1, x2, y2, x3, y3, r));
-    }
-
-    /**
-     * Testing with a valid input of three points that don not make a triangle which fits
-     * into/on a circle of radius one.
-     */
-    @Test
-    void threePoints_ValidInput_DoNotHaveTriangle() {
-        double x1 = 2;
-        double y1 = 0;
-        double x2 = 0;
-        double y2 = 3;
-        double x3 = -1;
-        double y3 = 0;
-        double r = 1;
-        assertFalse(LaunchInterceptorConditions.helperCircle(x1, y1, x2, y2, x3, y3, r));
-        x1 = 1.2;
-        y1 = 3.5;
-        x2 = 1.2;
-        y2 = 2.5;
-        x3 = 5.2;
-        y3 = 1.5;
-        assertFalse(LaunchInterceptorConditions.helperCircle(x1, y1, x2, y2, x3, y3, r));
-        x1 = 0.7071;
-        y1 = 0.7071;
-        x2 = -0.7;
-        y2 = 0.866;
-        x3 = -0.7071;
-        y3 = -0.7071;
-        assertFalse(LaunchInterceptorConditions.helperCircle(x1, y1, x2, y2, x3, y3, r));
-        x1 = 3.031;
-        y1 = 1.75;
-        x2 = 1.75;
-        y2 = 3.031;
-        x3 = -1.75;
-        y3 = -3.031;
-        r = 3.4;
-        assertFalse(LaunchInterceptorConditions.helperCircle(x1, y1, x2, y2, x3, y3, r));
-    }
-
     /**
      * Should return false if the first point coincides with the vertex
      */
@@ -329,7 +15,7 @@ public class LaunchInterceptorConditionsTest {
         double[] y = new double[]{0, 0, 0, 0, 0, 0, 0};
         int c_pts = 2;
         int d_pts = 2;
-        double epsilon = 1;
+        double epsilon = 0;
         boolean result = LaunchInterceptorConditions.condition9(x, y, c_pts, d_pts, epsilon, 7);
         assertEquals(result, false);
     }
@@ -343,7 +29,7 @@ public class LaunchInterceptorConditionsTest {
         double[] y = new double[]{0, 0, 0, 0, 0, 0, 0};
         int c_pts = 2;
         int d_pts = 2;
-        double epsilon = 1;
+        double epsilon = 0;
         boolean result = LaunchInterceptorConditions.condition9(x, y, c_pts, d_pts, epsilon, 7);
         assertEquals(result, false);
     }
@@ -353,11 +39,11 @@ public class LaunchInterceptorConditionsTest {
      */
     @Test
     void co9_3() {
-        double[] x = new double[] {1, 2, 3, 4, 5, 6, 7};
-        double[] y = new double[] {0, 0, 0, 0, 0, 0, 0};
+        double[] x = new double[] {0, 1, 2, 3, 4, 5, 6};
+        double[] y = new double[] {0, 0, 0, 0, 0, 0, 1};
         int c_pts = 2;
         int d_pts = 2;
-        double epsilon = 1;
+        double epsilon = 0;
         boolean result = LaunchInterceptorConditions.condition9(x, y, c_pts, d_pts, epsilon, 7);
         assertEquals(result, true);
     }
@@ -371,7 +57,7 @@ public class LaunchInterceptorConditionsTest {
         double[] y = new double[]{0, 0, 0, 0};
         int c_pts = 1;
         int d_pts = 1;
-        double epsilon = 1;
+        double epsilon = 0;
         boolean result = LaunchInterceptorConditions.condition9(x, y, c_pts, d_pts, epsilon, 4);
         assertEquals(result, false);
     }
