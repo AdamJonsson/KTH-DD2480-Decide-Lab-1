b1aaffa2d810ecf7100de1b529cbb06bf22c0cc0
Hovig <hoffek@kth.se>
2021-02-02 16:24:48 +0100

    Add `numPoints` field to the InputData class

--- src/lab1/InputData.java
+++ src/lab1/InputData.java
@@ -1,6 +1,10 @@
 package lab1;
 
+/**
+ * A class for representing the input data.
+ */
 public class InputData {
+    public int numPoints;
     public double[] x;
     public double[] y;
     public String[][] lcm;
@@ -25,13 +29,18 @@ public class InputData {
     public int nPts;
     public int qPts;
 
-    /** There need to exist an empty constructor for JSON parsing */
-    public InputData() {}
+    /**
+     * There need to exist an empty constructor for JSON parsing
+     */
+    public InputData() {
+    }
 
-    /** This constructor is used to manually construct input data */
+    /**
+     * This constructor is used to manually construct input data
+     */
     public InputData(double[] x, double[] y, String[][] lcm, boolean[] puv, double length1, double length2,
-            double radius1, double radius2, double epsilon, double area1, double area2, int quads, double dist,
-            int aPts, int bPts, int cPts, int dPts, int ePts, int fPts, int gPts, int kPts, int nPts, int qPts) {
+                     double radius1, double radius2, double epsilon, double area1, double area2, int quads, double dist,
+                     int aPts, int bPts, int cPts, int dPts, int ePts, int fPts, int gPts, int kPts, int nPts, int qPts) {
         this.x = x;
         this.y = y;
         this.lcm = lcm;
